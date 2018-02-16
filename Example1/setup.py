d6681cc837b2ce5c0ded25f61e028f766fc37eb9
326487162 <Welcome@661>
2018-02-16 16:11:30 -0500

    Get long version string

--- Example1/setup.py
+++ Example1/setup.py
@@ -4,14 +4,15 @@ Example of how to use setuptools
 
 from setuptools import setup, find_packages
 
+# 命令行："python setup.py --version" 可以获得版本号。
 
-# from submodule1 import __version__      # 1) 从 submodule1.__init__.__version__ 获得版本号
-# __version__ == "0.0.1-dev"              # 2) 或直接在本地定义 version
+# from submodule1 import __version__                    # 1) 从 submodule1.__init__.__version__ 获得版本号
+# __version__ = "0.0.2"                                 # 2) 或直接在本地定义 version
 
 setup(
-    # 3) 或依据 tag 动态获得版本号(参考文档 <git release flow>)
-    version_command='git describe --always',     # 如果采用 1) 或 2) 方式，则使用指令 `version=__version__`
+    version_command='git describe --always --long --dirty=-dev',     # 3) 或依据 tag 动态获得版本号(参考文档 <git release flow>)
     # `--always` 如果没有打过标签会出现错误信息 `fatal: No names found, cannot describe anything.`，这个参数将返回 commit hash number 代替 tag 以避免错误.
+    # `--long --dirty=-dev` 获得长格式版本信息： <version>-<times>-<commit-hash>-<dirty> 例如：0.0.2-0-g00bd0b4-dev
 
     name = "mymodules",
     packages = find_packages(exclude=['tests', '*.tests', '*.tests.*']),
